6a4657f1cc495560d92ec5f63519f44041eee869
jdh4056 <jdh4056@naver.com>
2025-07-11 15:29:54 +0900

    0711/이분탐색 최대최소값 찾기

---
+++ binsearch/최대최소값 찾기/2805.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int n,m;
+int a[1000005];
+
+bool isPossible(int mid) {
+    long long sum = 0;
+    for (int i  =0; i < n; i++) {
+        if (a[i] > mid) {
+            sum += (a[i] - mid);
+        }
+    }
+
+    return sum >= m;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int st = 0;
+    int en = 0;
+    for (int i = 0; i < n; i++) en = max(en, a[i]);
+
+    int res = 0;
+    while (st <= en) {
+        int mid = (st + en) / 2;
+
+        if (isPossible(mid)) {
+            res = mid;
+            st = mid + 1;
+        } else {
+            en = mid - 1;
+        }
+    }
+
+    cout << res;
+}
