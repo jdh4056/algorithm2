4eb03d961946924cca98835a0ab381a0ac1018b9
jdh4056 <jdh4056@naver.com>
2025-07-14 18:15:28 +0900

    0714/특정위치/lower_bound

---
+++ binsearch/특정 위치/12015.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int n;
+int a[1000005];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    vector<int> lis;
+    for (int i = 0; i < n; i++) {
+        auto x = lower_bound(lis.begin(), lis.end(), a[i]);
+        if (x == lis.end()) lis.push_back(a[i]);
+        else *x = a[i];
+    }
+
+    cout << lis.size();
+}
