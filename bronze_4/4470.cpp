49888d5f5b306bfebea077a3ee4192d87020504f
jdh4056 <jdh4056@naver.com>
2025-07-24 00:21:38 +0900

    0722

---
+++ bronze_4/4470.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    cin.ignore();
+    for (int i = 0; i < n;i++) {
+        string s;
+        getline(cin, s);
+
+        cout << i+1 << ". " <<s << '\n';
+    }
+}
