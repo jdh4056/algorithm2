c0cc6700d788828e0aedf0bbd1306d9f0a18802a
jdh4056 <jdh4056@naver.com>
2025-08-13 19:46:31 +0900

    0813

---
+++ bronze_3/9848.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin >> n >> k;
+    int a[n];
+    for (int i = 0; i<n;i++) cin >> a[i];
+
+    int cnt = 0;
+    for (int i = 0; i<n-1;i++) {
+        if (a[i] - a[i+1] >= k) cnt++;
+    }
+
+    cout << cnt;
+}
